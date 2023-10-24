3e409565eedef01147d93ddce7da2a08cc70e9d7
Convert-Bair <117106188+sanchousser@users.noreply.github.com>
2023-10-24 19:36:17 +0300

    bugfix

--- src/js/product-slider.js
+++ src/js/product-slider.js
@@ -1,11 +1,13 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
+    slidesPerView: 1,
+    spaceBetween: 18,
   
     breakpoints: {
         375: {
           slidesPerView: 1,
-          spaceBetween: 0,
+          spaceBetween: 18,
         },
         768: {
           slidesPerView: 2.5,
